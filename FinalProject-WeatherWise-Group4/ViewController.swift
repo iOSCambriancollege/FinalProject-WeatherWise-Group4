65302d01713ea8ac8a41142c9e151a4622cf071a
majidpawar9 <majidpawar9@gmail.com>
2023-11-15 22:10:56 -0500

    init

---
+++ FinalProject-WeatherWise-Group4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProject-WeatherWise-Group4
+//
+//  Created by Majid Pawar on 2023-11-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
