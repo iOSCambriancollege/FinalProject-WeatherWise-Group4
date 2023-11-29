8a528e561fe0c728c66f88cf16a22276d274b74f
majidpawar9 <majidpawar9@gmail.com>
2023-11-28 21:48:34 -0500

    Implemented ForecastViewModel to segregate view and api

---
+++ FinalProject-WeatherWise-Group4/ForecastViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  ForecastViewModel.swift
+//  FinalProject-WeatherWise-Group4
+//
+//  Created by Majid Pawar on 2023-11-28.
+//
+
+import Foundation
+
+struct ForecastViewModel {
+    let forecast: Forecast.Daily
+    
+    private static var dateFormatter: DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, MMM, d"
+        return dateFormatter
+    }
+    
+    private static var numberFormatter: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.maximumFractionDigits = 0
+        return numberFormatter
+    }
+    
+    private static var numberFormatter2: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .percent
+        return numberFormatter
+    }
+    
+    var day: String {
+        return Self.dateFormatter.string(from: forecast.dt)
+    }
+    
+    var overview: String {
+        forecast.weather[0].description.capitalized
+    }
+    
+    var high: String {
+        return "H: \(Self.numberFormatter.string(for: forecast.temp.max) ?? "0")°"
+    }
+    
+    var low: String {
+        return "L: \(Self.numberFormatter.string(for: forecast.temp.min) ?? "0")°"
+    }
+    
+    var pop: String {
+        return "💧 \(Self.numberFormatter2.string(for: forecast.pop) ?? "0%")"
+    }
+    
+    var clouds: String {
+        return "☁️ \(forecast.clouds)%"
+    }
+    
+    var humidity: String {
+        return "Humidity: \(forecast.humidity)%"
+    }
+}
