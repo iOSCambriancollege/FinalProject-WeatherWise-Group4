e71175ed6536d226598ecdc756fe11b9e3ea4d89
majidpawar9 <majidpawar9@gmail.com>
2023-12-10 22:23:16 -0500

    Added error handling for disconnection

--- FinalProject-WeatherWise-Group4/ForecastListViewModel.swift
+++ FinalProject-WeatherWise-Group4/ForecastListViewModel.swift
@@ -9,10 +9,15 @@ import CoreLocation
 import Foundation
 import SwiftUI
 
-
 class ForecastListViewModel: ObservableObject {
+    struct AppError: Identifiable {
+        let id = UUID().uuidString
+        let errorString: String
+    }
     
     @Published var forecasts: [ForecastViewModel] = []
+    var appError: AppError? = nil
+    @Published var isLoading: Bool = false
     @AppStorage("location") var location: String = ""
     @AppStorage("system") var system: Int = 0 {
         didSet {
@@ -29,9 +34,12 @@ class ForecastListViewModel: ObservableObject {
     }
     
     func getWeatherForecast() {
+        isLoading = true
         let apiService = APIService.shared
         CLGeocoder().geocodeAddressString(location) { (placemarks, error) in
             if let error = error {
+                self.isLoading = false
+                self.appError = AppError(errorString: error.localizedDescription)
                 print(error.localizedDescription)
             }
             if let lat = placemarks?.first?.location?.coordinate.latitude,
@@ -39,15 +47,17 @@ class ForecastListViewModel: ObservableObject {
                 // Don't forget to use your own key
                         apiService.getJSON(urlString: "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=current,minutely,hourly,alerts&appid=87db448e4dac72ac4fc7b9d5844fb89c",
                                            dateDecodingStrategy: .secondsSince1970) { (result: Result<Forecast,APIService.APIError>) in
-
                     switch result {
                     case .success(let forecast):
                         DispatchQueue.main.async {
+                            self.isLoading = false
                             self.forecasts = forecast.daily.map { ForecastViewModel(forecast: $0, system: self.system)}
                         }
                     case .failure(let apiError):
                         switch apiError {
                         case .error(let errorString):
+                            self.isLoading = false
+                            self.appError = AppError(errorString: errorString)
                             print(errorString)
                         }
                     }
@@ -56,4 +66,5 @@ class ForecastListViewModel: ObservableObject {
         }
 
     }
+    
 }
